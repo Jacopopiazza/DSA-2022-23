c439d4b4988b7f2dcfc24c5efd8b2057953d8501
JPiazza <jacopo@ideologica.it>
2023-09-06 12:01:24 +0200

    Initial Commit

---
+++ ProgettoApi2023/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ProgettoApi2023
+//
+//  Created by Japo on 06/09/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
